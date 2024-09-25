747e466177f8a554cde63ec5c4050ab7f716fa34
Rodriguez-Alfonso <rodriguezcalfonso1@gmail.com>
2024-09-24 21:31:45 -0700

    init repo

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync sequelize models to the database, then start the server
+const startServer = async () => {
+  try {
+    await sequelize.sync({ force: false });
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+startServer();
